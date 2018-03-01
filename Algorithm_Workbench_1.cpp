175e71ab9f4b0703e368ed8844b8e630720ff6c6
Damon Nutter <36419082+NutterDamon@users.noreply.github.com>
2018-03-01 13:31:31 -0800

    Update Algorithm_Workbench_1.cpp

--- Algorithm_Workbench_1.cpp
+++ Algorithm_Workbench_1.cpp
@@ -7,40 +7,40 @@ binary representation of the integer.
 #include <string>
 using namespace std;
 const int BASE = 2;
-string toBinary(int input);
+string toBinary(unsigned long long int input);
 int main() {
-	int input;
+	unsigned long long int input;
 	string converted;
-	cout << "Enter an integer up to a max of 536,870,912 and I will convert it into a binary string:";
+	cout << "Enter an integer up to a max of 10^16 and I will convert it into a binary string:";
 	cin >> input;
 	converted = toBinary(input);
 	cout << converted << endl;
 	system("pause");
 	return 0;
 }
-string toBinary(int input) {
-	string converted = ""; 		// setting up the string
-	bool noTrail= false;		// Not allowing trailing zeros
-	int spaceCounter = 0;		// counter for the 4 character spacing
-	for (int SIZE = 30; SIZE >= 0; SIZE--) {	// Loop through max possible size
-		int divisor = pow(BASE, SIZE);		// Set up the divisor 
-		if (input >= divisor) {			
-			int quotient = floor(input / divisor); //Round down quotient
-			converted += to_string(quotient);	// insert the quotient to string
-			int remainder = input % divisor;	// set up remainder
-			input = remainder;			// set remainder to remaining input
-			noTrail = true;				// Allowing trailing 0's
-			spaceCounter++;	
+string toBinary(unsigned long long int input) {
+	string converted = "";
+	bool noTrail= false;
+	int spaceCounter = 0;
+	for (int SIZE = 54; SIZE >= 0; SIZE--) {
+		unsigned long long int divisor = pow(BASE, SIZE);
+		if (input >= divisor) {
+			unsigned long long int quotient = floor(input / divisor);
+			converted += to_string(quotient);
+			int remainder = input % divisor;
+			input = remainder;
+			noTrail = true;
+			spaceCounter++;
 		}
-		else if (noTrail && input < divisor) {		// Checking if 0 needs to be inserted
+		else if (noTrail && input < divisor) {
 			converted += to_string(0);
 			spaceCounter++;
 		}
-		if (input == divisor) {				// Checking if 0 needs to be inserted
+		if (input == divisor) {
 			converted += to_string(0);
 			spaceCounter++;
 		}
-		if (spaceCounter == 4) {			//Adding left aligned 4 character spacing
+		if (spaceCounter == 4) {
 			converted += " ";
 			spaceCounter = 0;
 		}
